77b73edca6b32545e32edd34a4af453aed72687d
Ayush Shah <shahayush001@gmail.com>
2021-09-26 14:18:46 +0530

    initial commit

---
+++ scripts/speech.js
@@ -0,0 +1,45 @@
+function runSpeechRecognition(box) {
+    
+    var output = document.getElementById("output");
+    
+    var action = document.getElementById("action");
+    
+    var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+    var recognition = new SpeechRecognition();
+
+    recognition.onstart = function() {
+        action.innerHTML = "<small>listening, please speak...</small>";
+    };
+    
+    recognition.onspeechend = function() {
+        action.innerHTML = "<small>stopped listening, hope you are done! Click again to start.</small>";
+        recognition.stop();
+    }
+  
+    recognition.onresult = function(event) {
+        var transcript = event.results[0][0].transcript;
+        var confidence = event.results[0][0].confidence;
+        output.innerHTML = "<b>Text:</b> " + transcript.capitalize() + "<br> <b>Confidence:</b> " + confidence*100+"%";
+        output.classList.remove("hide");
+    };
+  
+recognition.start();
+}
+
+String.prototype.capitalize = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+}
+
+function textToAudio() {
+    let msg = document.getElementById("text-to-speech").value;
+    
+    let speech = new SpeechSynthesisUtterance();
+    speech.lang = "en-US";
+    
+    speech.text = msg;
+    speech.volume = 1;
+    speech.rate = 1;
+    speech.pitch = 1;
+    
+    window.speechSynthesis.speak(speech);
+}
